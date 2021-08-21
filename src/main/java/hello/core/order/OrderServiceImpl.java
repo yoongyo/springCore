c13a1bc07d858ec1d10e029fc9e19678fa61e879
yoongyo <jyg0172@naver.com>
2021-08-21 12:01:30 +0900

    @RequiredArgsContsructor

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,10 +6,12 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final이 붙은 필드를 모아서 생성자를 자동으로 만들어 준다. 
 public class OrderServiceImpl implements OrderService{
 
     // private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -21,11 +23,12 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    // RequiredArgsConstructor 가 기보적으로 생성자를 만들어준다.
+    //    @Autowired
+    //    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    //        this.memberRepository = memberRepository;
+    //        this.discountPolicy = discountPolicy;
+    //    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
