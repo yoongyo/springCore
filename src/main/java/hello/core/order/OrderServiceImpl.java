acc7255d379dac117023f0315fc29ca939d86378
yoongyo <jyg0172@naver.com>
2021-08-22 19:28:42 +0900

    make annotation

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -11,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final이 붙은 필드를 모아서 생성자를 자동으로 만들어 준다. 
+//@RequiredArgsConstructor // final 이 붙은 필드를 모아서 생성자를 자동으로 만들어 준다.
 public class OrderServiceImpl implements OrderService{
 
     // private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -23,12 +24,12 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    // RequiredArgsConstructor 가 기보적으로 생성자를 만들어준다.
-    //    @Autowired
-    //    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-    //        this.memberRepository = memberRepository;
-    //        this.discountPolicy = discountPolicy;
-    //    }
+//     RequiredArgsConstructor 가 기보적으로 생성자를 만들어준다.
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
